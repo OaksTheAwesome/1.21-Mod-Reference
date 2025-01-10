9580eef61eb516741577ccf8c06d90fd48bfacfe
OaksTheAwesome <oakstheawesome@gmail.com>
2025-01-10 15:39:41 -0600

    Added new items, edible, guidite material stats and sword item with appropriate attributes

---
+++ testmod-template-1.21/src/main/java/net/oakstheawesome/testmod/GuiditeMaterial.java
@@ -0,0 +1,44 @@
+package net.oakstheawesome.testmod;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.registry.tag.TagKey;
+
+public class GuiditeMaterial implements ToolMaterial {
+
+    @Override
+    public int getDurability() {
+        return 455;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 5.0F;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return 8.0F;
+    }
+
+    @Override
+    public TagKey<Block> getInverseTag() {
+        return BlockTags.INCORRECT_FOR_IRON_TOOL;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 22;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(ModItems.SUSPICIOUS_SUBSTANCE, Items.POTATO);
+    }
+
+    public static final GuiditeMaterial INSTANCE = new GuiditeMaterial();
+
+}
--- testmod-template-1.21/src/main/java/net/oakstheawesome/testmod/ModItems.java
+++ testmod-template-1.21/src/main/java/net/oakstheawesome/testmod/ModItems.java
@@ -1,17 +1,29 @@
 package net.oakstheawesome.testmod;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
+
+import java.util.List;
 
 
 //Registration method
 public class ModItems {
-    public static Item register(Item item, String id) {
+    public static Item register(String id, Item item) {
         Identifier itemID = Identifier.of(TestMod.MOD_ID, id);
         Item registeredItem = Registry.register(Registries.ITEM, itemID, item);
         return  registeredItem;
@@ -19,19 +31,49 @@ public class ModItems {
 
 
 //Add Mod Items here
-public static final Item SUSPICIOUS_SUBSTANCE = register(
-        new Item(new Item.Settings()),
-        "suspicious_substance");
+public static final Item SUSPICIOUS_SUBSTANCE = register("suspicious_substance", new Item(new Item.Settings()
+    .rarity(Rarity.UNCOMMON)) {
+    //Adds a tooltip to the item
+    @Override
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        tooltip.add(Text.translatable("tooltip.testmod.suspicious_substance.tooltip").formatted(Formatting.LIGHT_PURPLE));
+        super.appendTooltip(stack, context, tooltip, type);
+    }}); //Add next item beyond this point
+
+//Example of a food item
+public static final Item PEAR = register("pear", new Item(new Item.Settings()
+        .food(new FoodComponent.Builder()
+                .nutrition(4)
+                .statusEffect(new StatusEffectInstance(StatusEffects.ABSORPTION, 60 * 20, 1), 0.8f)
+                .saturationModifier(3)
+                .build())));
+
+//Example of a tool/weapon, values relative to material's stats (In this case, GuiditeMaterial stats. Ex attack dmg base is 8.0F=9 + 4 = 13 attack dmg total
+public static final Item GUIDITE_SWORD = register("guidite_sword", new SwordItem(GuiditeMaterial.INSTANCE, new Item.Settings()
+        .attributeModifiers(SwordItem.createAttributeModifiers(GuiditeMaterial.INSTANCE, 4, -2.4f))));
 
 
 
 //Initialize Mod Items and register items to groups
 public static void initialize(){
     ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
-            .register(fabricItemGroupEntries -> fabricItemGroupEntries
-                    .add(ModItems.SUSPICIOUS_SUBSTANCE));
-    }
+                    .register(fabricItemGroupEntries -> fabricItemGroupEntries
+                            .add(ModItems.SUSPICIOUS_SUBSTANCE));
 
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK)
+                    .register(fabricItemGroupEntries -> fabricItemGroupEntries
+                            .add(ModItems.PEAR));
+
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT)
+                    .register(fabricItemGroupEntries -> fabricItemGroupEntries
+                            .add(ModItems.GUIDITE_SWORD));
+
+    FuelRegistry.INSTANCE
+            .add(ModItems.SUSPICIOUS_SUBSTANCE, 30 * 20);
+
+    CompostingChanceRegistry.INSTANCE
+            .add(ModItems.SUSPICIOUS_SUBSTANCE, 0.3f);
+    }
 
 }
 
