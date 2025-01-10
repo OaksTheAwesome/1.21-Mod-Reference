edfb146db32eba3ff4394f8cb5fa5a6275851578
OaksTheAwesome <oakstheawesome@gmail.com>
2025-01-10 12:48:20 -0600

    Item basis

--- testmod-template-1.21/src/main/java/net/oakstheawesome/testmod/ModItems.java
+++ testmod-template-1.21/src/main/java/net/oakstheawesome/testmod/ModItems.java
@@ -1,6 +1,9 @@
 package net.oakstheawesome.testmod;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -15,4 +18,21 @@ public class ModItems {
     }
 
 
+//Add Mod Items here
+public static final Item SUSPICIOUS_SUBSTANCE = register(
+        new Item(new Item.Settings()),
+        "suspicious_substance");
+
+
+
+//Initialize Mod Items and register items to groups
+public static void initialize(){
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+            .register(fabricItemGroupEntries -> fabricItemGroupEntries
+                    .add(ModItems.SUSPICIOUS_SUBSTANCE));
+    }
+
+
 }
+
+
--- testmod-template-1.21/src/main/java/net/oakstheawesome/testmod/TestMod.java
+++ testmod-template-1.21/src/main/java/net/oakstheawesome/testmod/TestMod.java
@@ -15,9 +15,7 @@ public class TestMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		ModItems.initialize();
 
 		LOGGER.info("Hello Fabric world!");
 	}
